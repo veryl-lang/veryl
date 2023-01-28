f27c51b563cdb6970667630511ef7cdd16f2bc1a
dalance <dalance@gmail.com>
2023-01-28 13:27:50 +0900

    Fix wasm panic #108

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,6 +1,7 @@
 use miette::{ErrReport, GraphicalReportHandler, GraphicalTheme, ThemeCharacters, ThemeStyles};
 use semver::Version;
 use std::collections::HashMap;
+use veryl_analyzer::Analyzer;
 use veryl_emitter::Emitter;
 use veryl_formatter::Formatter;
 use veryl_metadata::{Build, Format, Metadata, Project};
@@ -65,6 +66,11 @@ pub fn parse(source: &str) -> ParseResult {
     let metadata = metadata();
     match Parser::parse(source, &"") {
         Ok(parser) => {
+            let analyzer = Analyzer::new::<&str>(&[]);
+            let _ = analyzer.analyze_pass1(source, &"", &parser.veryl);
+            let _ = analyzer.analyze_pass2(source, &"", &parser.veryl);
+            let _ = analyzer.analyze_pass3(source, &"", &parser.veryl);
+
             let mut emitter = Emitter::new(&metadata);
             emitter.emit(&parser.veryl);
             ParseResult {
@@ -95,5 +101,37 @@ pub fn format(source: &str) -> ParseResult {
             code: "".to_owned(),
             err: render_err(e.into()),
         },
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test() {
+        let path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+        let mut path = PathBuf::from(path);
+        path.push("playground");
+        path.push("index.html");
+        let text = std::fs::read_to_string(path).unwrap();
+        let mut code = false;
+        let mut code_text = String::new();
+        for line in text.lines() {
+            if line.contains("</textarea") {
+                code = false;
+            }
+            if code {
+                code_text.push_str(&format!("{line}\n"));
+            }
+            if line.contains("<textarea") {
+                code = true;
+            }
+        }
+        let ret = parse(&code_text);
+
+        assert_eq!(ret.err, "");
+        assert_ne!(ret.code, "");
     }
 }
