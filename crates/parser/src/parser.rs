19e3c3bde9cc147906def6651da377673fb913d7
dalance <dalance@gmail.com>
2023-02-10 09:43:30 +0900

    Fix panic at empty source code

--- crates/parser/src/parser.rs
+++ crates/parser/src/parser.rs
@@ -1,4 +1,5 @@
 use crate::parser_error::ParserError;
+use crate::resource_table;
 use crate::veryl_grammar::VerylGrammar;
 use crate::veryl_grammar_trait::Veryl;
 use crate::veryl_parser::parse;
@@ -12,6 +13,9 @@ pub struct Parser {
 impl Parser {
     #[allow(clippy::result_large_err)]
     pub fn parse<T: AsRef<Path>>(input: &str, file: &T) -> Result<Self, ParserError> {
+        // Inserting PathId because it will not be inserted if input doesn't have token.
+        let _ = resource_table::insert_path(file.as_ref());
+
         let mut grammar = VerylGrammar::new();
         parse(input, file, &mut grammar)?;
 
