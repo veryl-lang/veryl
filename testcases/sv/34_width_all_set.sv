81b3e73981b944ae286d988e4c1bbe3c771a539b
dalance <dalance@gmail.com>
2023-03-15 09:41:32 +0900

    Add sized set bits

---
+++ testcases/sv/34_width_all_set.sv
@@ -0,0 +1,10 @@
+module veryl_testcase_Module34;
+    localparam int unsigned a0 = '0;
+    localparam int unsigned a1 = '1;
+    localparam int unsigned a2 = 'x;
+    localparam int unsigned a3 = 'z;
+    localparam int unsigned a4 = 10'b0000000000;
+    localparam int unsigned a5 = 10'b1111111111;
+    localparam int unsigned a6 = 10'bxxxxxxxxxx;
+    localparam int unsigned a7 = 10'bzzzzzzzzzz;
+endmodule
