48da9b7a2b088bce874aedfc747671eec8039b0a
Nathan <nbleier3@illinois.edu>
2023-07-31 10:34:32 -0500

    added 38_typedef.sv

---
+++ testcases/sv/38_typedef.sv
@@ -0,0 +1,10 @@
+module veryl_testcase_Module38;
+    typedef logic        [16-1:0]         word_t   ;
+    typedef logic        [16-1:0][16-1:0] words_t  ;
+    typedef word_t                         regfile_t [0:16-1];
+    typedef 
+    bit     [8-1:0] octbyte [0:8-1];
+
+    regfile_t rf   ;
+    assign rf[0] = '0;
+endmodule
