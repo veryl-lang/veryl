012dd1a9631e464ad73ba7a784542852da275c2a
dalance <dalance@gmail.com>
2024-03-04 23:17:01 +0900

    Fix default metadata

--- crates/metadata/src/doc.rs
+++ crates/metadata/src/doc.rs
@@ -1,11 +1,19 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
-#[derive(Clone, Debug, Default, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct Doc {
     #[serde(default = "default_path")]
     pub path: PathBuf,
+}
+
+impl Default for Doc {
+    fn default() -> Self {
+        Self {
+            path: default_path(),
+        }
+    }
 }
 
 fn default_path() -> PathBuf {
