5cf7513e51181a4eb4d98dc4c0a0749b2706c215
dalance <dalance@gmail.com>
2024-05-27 09:51:19 +0900

    Add testcase for #692

---
+++ testcases/sv/59_same_name.sv
@@ -0,0 +1,7 @@
+package veryl_testcase_Package59A;
+    localparam int unsigned XLEN = 32;
+endpackage
+
+package veryl_testcase_Package59B;
+    localparam int unsigned XLEN = veryl_testcase_Package59A::XLEN;
+endpackage
