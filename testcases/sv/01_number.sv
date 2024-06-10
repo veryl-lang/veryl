42f352b1ab79e303100fbc4a5af8e415da365793
dalance <dalance@gmail.com>
2024-06-10 16:53:05 +0900

    Add signed literal support

--- testcases/sv/01_number.sv
+++ testcases/sv/01_number.sv
@@ -4,20 +4,24 @@ module veryl_testcase_Module01;
     localparam int unsigned aa = 01234_56789;
 
     // binary
-    localparam int unsigned b  = 32'b01xzXZ;
-    localparam int unsigned bb = 32'b01_xz_XZ;
+    localparam int unsigned b   = 32'b01xzXZ;
+    localparam int unsigned bb  = 32'b01_xz_XZ;
+    localparam int unsigned bbb = 32'sb01_xz_XZ;
 
     // octal
-    localparam int unsigned c  = 32'o01234567xzXZ;
-    localparam int unsigned cc = 32'o01234_567xzXZ;
+    localparam int unsigned c   = 32'o01234567xzXZ;
+    localparam int unsigned cc  = 32'o01234_567xzXZ;
+    localparam int unsigned ccc = 32'so01234_567xzXZ;
 
     // decimal
-    localparam int unsigned d  = 32'd0123456789;
-    localparam int unsigned dd = 32'd01234_56789;
+    localparam int unsigned d   = 32'd0123456789;
+    localparam int unsigned dd  = 32'd01234_56789;
+    localparam int unsigned ddd = 32'sd01234_56789;
 
     // hex
-    localparam int unsigned e  = 128'h0123456789abcdefxzABCDEFXZ;
-    localparam int unsigned ee = 128'h01234_5678_9abc_defxz_ABCD_EFXZ;
+    localparam int unsigned e   = 128'h0123456789abcdefxzABCDEFXZ;
+    localparam int unsigned ee  = 128'h01234_5678_9abc_defxz_ABCD_EFXZ;
+    localparam int unsigned eee = 128'sh01234_5678_9abc_defxz_ABCD_EFXZ;
 
     // all0, all1, allx, allz
     localparam int unsigned f      = '0;
