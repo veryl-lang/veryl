b5f964dc4d5617319f1100980181333c6d618824
dalance <dalance@gmail.com>
2024-08-21 12:40:41 +0900

    Update script

--- support/scripts/tag_submodule.sh
+++ support/scripts/tag_submodule.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-sub=("doc" "crates/metadata/std")
-root=`git rev-parse --show-toplevel`
+if [ "$DRY_RUN" = "true" ]; then
+    echo "tagging skipped"
+else
+    sub=("doc" "crates/metadata/std")
+    root=`git rev-parse --show-toplevel`
 
-pushd ${root}
+    pushd ${root}
 
-for dir in ${sub[@]}
-do
-    pushd ${dir}
-    git tag -f v$1
-    git push origin -f v$1
+    for dir in ${sub[@]}
+    do
+        pushd ${dir}
+        git tag -f v$1
+        git push origin -f v$1
+        popd
+    done
+
     popd
-done
-
-popd
+fi
