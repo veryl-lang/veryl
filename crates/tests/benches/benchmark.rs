5be9bb8b06df1a7e16aadd1fba4d9ef22afe09ff
dalance <dalance@gmail.com>
2024-12-26 21:53:02 +0900

    Remove analyzer error from benchmark

--- crates/tests/benches/benchmark.rs
+++ crates/tests/benches/benchmark.rs
@@ -8,7 +8,12 @@ use veryl_parser::Parser;
 #[cfg(target_os = "linux")]
 mod perf;
 
-const EXCLUDES: [&str; 2] = [r"52_include.veryl", r"67_cocotb.veryl"];
+const EXCLUDES: [&str; 4] = [
+    r"25_dependency.veryl",
+    r"52_include.veryl",
+    r"67_cocotb.veryl",
+    r"68_std.veryl",
+];
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut text = String::new();
@@ -22,6 +27,20 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     let metadata_path = Metadata::search_from_current().unwrap();
     let metadata = Metadata::load(&metadata_path).unwrap();
+
+    // Check no analyzer error
+    let parser = Parser::parse(&text, &"").unwrap();
+    let prj = &metadata.project.name;
+    let analyzer = Analyzer::new(&metadata);
+    let mut errors = Vec::new();
+    errors.append(&mut analyzer.analyze_pass1(prj, &text, &"", &parser.veryl));
+    Analyzer::analyze_post_pass1();
+    errors.append(&mut analyzer.analyze_pass2(prj, &text, &"", &parser.veryl));
+    errors.append(&mut analyzer.analyze_pass3(prj, &text, &"", &parser.veryl));
+    if !errors.is_empty() {
+        dbg!(errors);
+        assert!(false);
+    }
 
     let mut group = c.benchmark_group("throughput");
     group.throughput(Throughput::Bytes(text.len() as u64));
