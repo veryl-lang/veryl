a44a5e9e9be520003e59f759c397652b88f61695
dalance <dalance@gmail.com>
2026-03-20 21:50:33 +0900

    Change bigint crate to num-bigint

--- crates/analyzer/src/lib.rs
+++ crates/analyzer/src/lib.rs
@@ -34,9 +34,9 @@ mod tests;
 
 type HashMap<K, V> = fxhash::FxHashMap<K, V>;
 type HashSet<V> = fxhash::FxHashSet<V>;
-type BigInt = malachite_bigint::BigInt;
-type BigUint = malachite_bigint::BigUint;
-type Sign = malachite_bigint::Sign;
+type BigInt = num_bigint::BigInt;
+type BigUint = num_bigint::BigUint;
+type Sign = num_bigint::Sign;
 
 pub use smallvec::smallvec as svec;
 pub type SVec<T> = smallvec::SmallVec<[T; 4]>;
